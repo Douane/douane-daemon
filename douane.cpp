f04dc8ea9f2ec5d4ec6f4e3acdd1dd7aa69b7b5e
Guillaume Hain <zedtux@zedroot.org>
2014-03-09 18:53:13 +0100

    Add debugs in Douane class (DBus)

--- douane.cpp
+++ douane.cpp
@@ -10,17 +10,21 @@ Douane::Douane(DBus::Connection &connection)
 
 Douane::~Douane(void)
 {
-
+  LOG4CXX_DEBUG(logger, "Douane::~Douane...");
 }
 
 void Douane::set_rules_manager(RulesManager * rules_manager)
 {
+  LOG4CXX_DEBUG(logger, "Douane::set_rules_manager...");
   this->rules_manager = rules_manager;
+  LOG4CXX_DEBUG(logger, "Douane::set_rules_manager done");
 }
 
 std::vector< ::DBus::Struct< std::string, std::string, bool > > Douane::GetRules()
 {
+  LOG4CXX_DEBUG(logger, "Douane::GetRules | this->rules_manager->get_valid_rules()...");
   const std::map<std::string, const Rule> valid_rules = this->rules_manager->get_valid_rules();
+  LOG4CXX_DEBUG(logger, "Douane::GetRules | this->rules_manager->get_valid_rules() Done");
 
   std::vector< ::DBus::Struct< std::string, std::string, bool > > rules;
   for(std::map<std::string, const Rule>::const_iterator it = valid_rules.begin(); it != valid_rules.end(); ++it)
@@ -30,7 +34,9 @@ std::vector< ::DBus::Struct< std::string, std::string, bool > > Douane::GetRules
     rule._2 = it->second.process_path;
     rule._3 = it->second.is_allowed();
 
+    LOG4CXX_DEBUG(logger, "Douane::GetRules | rules.push_back()...");
     rules.push_back(rule);
+    LOG4CXX_DEBUG(logger, "Douane::GetRules | rules.push_back() Done");
   }
 
   return rules;
